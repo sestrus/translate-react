5430cd5c9cd406311d36bad05d1d8f48de0220e3
Piotr <piotr.burdzy.klb@gmail.com>
2022-09-05 14:00:04 +0200

    adding ui draft

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Translate from "./Translate";
 
+const options = [
+  {
+    label: "Polish",
+    value: "pl",
+  },
+  {
+    label: "German",
+    value: "de",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+];
+
 const App = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+
   return (
-    <div>
-      <Translate />
+    <div className="ui container">
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Translate
+        options={options}
+        language={language}
+        onLanguageChange={setLanguage}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <div>Translated output</div>
     </div>
   );
 };
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const Translate = () => {
-  return <div>Translate</div>;
+const Translate = ({ language, onLanguageChange, options }) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick);
+    return () => {
+      return document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
+  const ref = useRef();
+  const renderedOptions = options.map((option) => {
+    if (option.value === language.value) {
+      return;
+    }
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          onLanguageChange(option);
+        }}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label>Choose Language</label>
+        <div
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{language.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Translate;
